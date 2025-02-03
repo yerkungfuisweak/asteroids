97a811f933a2a6e22f4ac1db032c991adf799034
yerkungfuisweak <yerkungfuisweak@gmail.com>
2025-02-03 00:15:07 +0000

    added forward and backwards movement with w and s

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -29,3 +29,11 @@ class Player(CircleShape):
             self.rotate(-dt)
         elif keys[pygame.K_d]:
             self.rotate(dt)
+        elif keys[pygame.K_w]:
+            self.move(dt)
+        elif keys[pygame.K_s]:
+            self.move(-dt)
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
